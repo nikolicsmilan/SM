7c903a35e5e9e4a998618d134c187423477c45c4
nikolicsmilan <milan.nikolics@weborigo.eu>
2023-06-10 17:38:45 +0200

    Check commit

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -8,6 +8,7 @@ const Info = () => {
         Az hogy ez mennyire zöld nem releváns!
         A github csak a public repositorykat és a main branchet figyeli, tehát semmi komolyabbat.
         Így akár napi 5 console.log meg lehet téveszteni másokat.
+        Check commit
     </div>
   )
 }
