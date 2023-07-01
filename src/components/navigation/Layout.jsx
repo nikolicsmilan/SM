c2b0b7e4602cc888b2179bbdc58c50ab66d25eb3
nikolicsmilan <nikolicsmilan@gmail.com>
2023-07-01 21:41:17 +0200

    reset the correct layout mobile view when load

--- src/components/navigation/Layout.jsx
+++ src/components/navigation/Layout.jsx
@@ -7,23 +7,22 @@ import useWindowSize from "../../hooks/use-windowsize";
 import { useStyleContext } from "../../context/StyleContext";
 
 const Layout = () => {
-  const { width, height } = useWindowSize();
+  const { width,height } = useWindowSize();
   const { activeAside, setActiveAside } = useStyleContext();
   return (
     <>
-      {width < 667 || width < height || 300 > height ? (
-        <div className="flex flex-col w-full  border-0 border-lime-400 m-0 p-0 meret">
-        
-        <div className="bg-white">
+      { width < 667 || width < height || 300 > height? (
+        <div className="flex flex-col w-full">
+          <div className="bg-white">
             <NavBar />
           </div>
 
           {!activeAside && (
-            <div className="w-full h-full fixed  bg-secondary z-50 opacity-95">
+            <div className="w-full fixed  bg-secondary z-50 h-full opacity-95">
               <Aside />
             </div>
           )}
-          <div className="flex-1 flex items-center justify-center border-0 border-red-400 ">
+          <div className="w-full ">
             <Outlet
               onClick={() => {
                 console.log("hozé");
@@ -31,16 +30,16 @@ const Layout = () => {
             />
           </div>
 
-          <footer class="bg-secondary fixed bottom-0 w-full h-12 flex justify-center ">
-            <BottomNavBar />
-          </footer>
+          <footer class="fixed bottom-0 w-full p-0 bg-secondary">
+              <BottomNavBar />
+            </footer>
         </div>
       ) : (
         <div className="flex flex-col w-full ">
           <div className="bg-white">
-            <NavBar />
+          <NavBar />
           </div>
-
+        
           <div className="flex">
             <div className=" flex w-1/6 bg-secondary  ">
               <Aside />
@@ -48,6 +47,7 @@ const Layout = () => {
             <div className="w-5/6 bg-success">
               <Outlet />
             </div>
+          
           </div>
         </div>
       )}
