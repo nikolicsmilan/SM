2a69d04b060b57e0d5e1fb94f5b10bf83d099672
nikolicsmilan <nikolicsmilan@gmail.com>
2023-08-03 19:14:03 +0200

    chnage direction to simple

--- src/components/shared/Input.jsx
+++ src/components/shared/Input.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 const Input = ({ label, placeholder, value, onChange, ...rest }) => {
   return (
     <div className="mb-4">
+      Azt szeretném elérni hogy paraméterezhető legyen az input, ha nem küldöm át ne jelenjen meg
+      és az értékeknél se legyen NAN
       <label className="block mb-2 text-sm font-bold text-gray-700">{label}</label>
       <input
         className="appearance-none w-full py-2 px-3 leading-tight rounded-lg shadow-md focus:outline-none focus:shadow-outline"
-        type="range"
+        type="text"
         min="0"
         max="360"
         placeholder={placeholder}
--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -45,66 +45,52 @@ const DashBoard = () => {
           </div>
         ))}
       </div>
-<h1>Az van hogy hagyom ezt a 3d dolgot mert túl sok időt elvesz a css el való viaskodás,
-  helyette a react kódot fogom folytatni. Lesz egy lista és egy csoport nézet
-  a listában minden elem látható lesz táblázatba rendezve.
-  A csoport nézet gombokkal való választgatást jelenti. Lesz egy újn elem felvitelle
-képfeltöltéssel és alap adatok felvitelével az adatbázisban. valamint a kiválasztott elem
-szerkesztése és törlése ( nem a csoporté hanem az adott elemé) Új csoportott nem
-tervezek egyenlőre megvalósítani az statikusan az adatbázisból fog eredni hogy milyen
-kategóriák vagy msánéven csoportok vannak. Egyenlőre ennyit erre a projektre.
+      <h1>
+        Az van hogy hagyom ezt a 3d dolgot mert túl sok időt elvesz a css el
+        való viaskodás, helyette a react kódot fogom folytatni. Lesz egy lista
+        és egy csoport nézet a listában minden elem látható lesz táblázatba
+        rendezve. A csoport nézet gombokkal való választgatást jelenti. Lesz egy
+        újn elem felvitelle képfeltöltéssel és alap adatok felvitelével az
+        adatbázisban. valamint a kiválasztott elem szerkesztése és törlése ( nem
+        a csoporté hanem az adott elemé) Új csoportott nem tervezek egyenlőre
+        megvalósítani az statikusan az adatbázisból fog eredni hogy milyen
+        kategóriák vagy msánéven csoportok vannak. Egyenlőre ennyit erre a
+        projektre.
+      </h1>
 
-</h1>
+      <h1>
+        Először a feltöltő részét kell megvalósítani, nem kellenek az
+        alcsoportok mert az ügfél képtelen őket meghatározni és én sme vagyok
+        gondolatolvasó szóval
+      </h1>
+
+      <ul>
+        <li>Név</li>
+        <li>Kép</li>
+        <li>Ár</li>
+        <li>Leírás</li>
+        <li>Kategória</li>
+       Plusz jel alatt, a config stateből ból fog jönni hogy mi jelenjen meg
+       az admin oldalon
+      </ul>
       <div className="card-container border-2 flex">
         <div className="card">
           <div className="card-front w-full h-full border-2">
             {/* Előlap tartalma */}Előlap
           </div>
           <div className="card-back w-32 h-32 border-2">
-            {/* Hátlap tartalma */}Hátlap
-          </div>
-        </div>
-        <div className="flex border-2">
-          <div className="w-16 m-1 mx-5">
-            <Input
+            {/* Hátlap tartalma  <Input
               label="side.oldal_1.x:"
               placeholder="X"
               value={side.oldal_1.x}
               onChange={(event) => handleInputChange(event, "oldal_1", "x")}
-            />
-            <Input
-              label="side.oldal_1.y:"
-              placeholder="Y"
-              value={side.oldal_1.y}
-              onChange={(event) => handleInputChange(event, "oldal_1", "y")}
-            />
-            <Input
-              label="side.oldal_1.z:"
-              placeholder="Z"
-              value={side.oldal_1.z}
-              onChange={(event) => handleInputChange(event, "oldal_1", "z")}
-            />
+            /> */}
+            Hátlap
           </div>
-          <div className="w-16 m-1 mx-5">
-            <Input
-              label="side.oldal_2.x:"
-              placeholder="X"
-              value={side.oldal_2.x}
-              onChange={(event) => handleInputChange(event, "oldal_2", "x")}
-            />
-            <Input
-              label="side.oldal_2.y:"
-              placeholder="Y"
-              value={side.oldal_2.y}
-              onChange={(event) => handleInputChange(event, "oldal_2", "y")}
-            />
-            <Input
-              label="side.oldal_2.z:"
-              placeholder="Z"
-              value={side.oldal_2.z}
-              onChange={(event) => handleInputChange(event, "oldal_2", "z")}
-            />
-          </div>
+        </div>
+        <div className="flex border-2">
+          <div className="w-16 m-1 mx-5"></div>
+          <div className="w-16 m-1 mx-5"></div>
 
           <div className="w-16 m-1 mx-5">
             <Input
@@ -113,181 +99,12 @@ kategóriák vagy msánéven csoportok vannak. Egyenlőre ennyit erre a projektr
               value={side.oldal_3.x}
               onChange={(event) => handleInputChange(event, "oldal_3", "x")}
             />
-            <Input
-              label="side.oldal_3.y:"
-              placeholder="Y"
-              value={side.oldal_3.y}
-              onChange={(event) => handleInputChange(event, "oldal_3", "y")}
-            />
-            <Input
-              label="side.oldal_3.z:"
-              placeholder="Z"
-              value={side.oldal_3.z}
-              onChange={(event) => handleInputChange(event, "oldal_3", "z")}
-            />
           </div>
 
-          <div className="w-16 m-1 mx-5">
-            <Input
-              label="side.oldal_4.x:"
-              placeholder="X"
-              value={side.oldal_4.x}
-              onChange={(event) => handleInputChange(event, "oldal_4", "x")}
-            />
-            <Input
-              label="side.oldal_4.y:"
-              placeholder="Y"
-              value={side.oldal_4.y}
-              onChange={(event) => handleInputChange(event, "oldal_4", "y")}
-            />
-            <Input
-              label="side.oldal_4.z:"
-              placeholder="Z"
-              value={side.oldal_4.z}
-              onChange={(event) => handleInputChange(event, "oldal_4", "z")}
-            />
-          </div>
+          <div className="w-16 m-1 mx-5"></div>
         </div>
-        <div className="w-16 m-1 mx-5">
-          <Input
-            label="side.oldal_5.x:"
-            placeholder="X"
-            value={side.oldal_5.x}
-            onChange={(event) => handleInputChange(event, "oldal_5", "x")}
-          />
-          <Input
-            label="side.oldal_5.y:"
-            placeholder="Y"
-            value={side.oldal_5.y}
-            onChange={(event) => handleInputChange(event, "oldal_5", "y")}
-          />
-          <Input
-            label="side.oldal_5.z:"
-            placeholder="Z"
-            value={side.oldal_5.z}
-            onChange={(event) => handleInputChange(event, "oldal_5", "z")}
-          />
-        </div>
-        <div className="w-16 m-1 mx-5">
-          <Input
-            label="side.oldal_6.x:"
-            placeholder="X"
-            value={side.oldal_6.x}
-            onChange={(event) => handleInputChange(event, "oldal_6", "x")}
-          />
-          <Input
-            label="side.oldal_6.y:"
-            placeholder="Y"
-            value={side.oldal_6.y}
-            onChange={(event) => handleInputChange(event, "oldal_6", "y")}
-          />
-          <Input
-            label="side.oldal_6.z:"
-            placeholder="Z"
-            value={side.oldal_6.z}
-            onChange={(event) => handleInputChange(event, "oldal_6", "z")}
-          />
-        </div>
-      </div>
-
-      <div className="my-cube">
-        <div className="top">
-          aaaaaaaaa bbbbbbbbbbbbbbbbbb cccccccccccccccccccc
-        </div>
-        <div className="cube">
-          <span
-            className="cube-span egy rounded-2xl "
-            style={{
-              "--i": 0,
-              "--y": `${side.oldal_1.y}deg`,
-              "--x": `${side.oldal_1.x}deg`,
-              "--z": `${side.oldal_1.z}px`,
-            }}
-          >
-         
-            <span className=" belso rounded-2xl"  style={{
-              "--i": 0,
-              "--y": `${side.oldal_6.y}deg`,
-              "--x": `${side.oldal_6.x}deg`,
-              "--z": `${side.oldal_6.z}px`,
-            }}>
-ez mi??
-            </span>
-
-            <span className=" belso rounded-2xl"  style={{
-              "--i": 0,
-              "--y": `${side.oldal_6.y}deg`,
-              "--x": `${side.oldal_6.x}deg`,
-              "--z": `${side.oldal_6.z}px`,
-            }}>
-ez mi??
-            </span>
-            <span className=" belso rounded-2xl"  style={{
-              "--i": 0,
-              "--y": `${side.oldal_6.y}deg`,
-              "--x": `${side.oldal_6.x}deg`,
-              "--z": `${side.oldal_6.z}px`,
-            }}>
-ez mi??
-            </span>
-          </span>
-          <span
-            className="cube-span ketto rounded-2xl invisible"
-            style={{
-              "--i": 1,
-              "--y": `${side.oldal_2.y}deg`,
-              "--x": `${side.oldal_2.x}deg`,
-              "--z": `${side.oldal_2.z}px`,
-            }}
-          >
-            Kettő
-          </span>
-          <span
-            className="cube-span harom invisible"
-            style={{
-              "--i": 2,
-              "--y": `${side.oldal_3.y}deg`,
-              "--x": `${side.oldal_3.x}deg`,
-              "--z": `${side.oldal_3.z}px`,
-            }}
-          >
-            Három
-          </span>
-          <span
-            className="cube-span negy invisible"
-            style={{
-              "--i": 3,
-              "--y": `${side.oldal_4.y}deg`,
-              "--x": `${side.oldal_4.x}deg`,
-              "--z": `${side.oldal_4.z}px`,
-            }}
-          >
-            Négy
-          </span>
-
-          <span
-            className="cube-span ot invisible"
-            style={{
-              "--i": 4,
-              "--y": `${side.oldal_5.y}deg`,
-              "--x": `${side.oldal_5.x}deg`,
-              "--z": `${side.oldal_5.z}px`,
-            }}
-          >
-            Öt
-          </span>
-          <span
-            className="cube-span hat invisible"
-            style={{
-              "--i": 5,
-              "--y": `${side.oldal_6.y}deg`,
-              "--x": `${side.oldal_6.x}deg`,
-              "--z": `${side.oldal_6.z}px`,
-            }}
-          >
-            Hat
-          </span>
-        </div>
+        <div className="w-16 m-1 mx-5"></div>
+        <div className="w-16 m-1 mx-5"></div>
       </div>
     </div>
   );
