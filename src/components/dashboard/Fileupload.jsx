eb350220c5269ec9acfbbe310af42a6c6f47c071
nikolicsmilan <nikolicsmilan@gmail.com>
2023-08-19 16:59:24 +0200

    create button for delete images from storage if cancel

--- src/components/dashboard/Fileupload.jsx
+++ src/components/dashboard/Fileupload.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { getDownloadURL, ref, uploadBytesResumable } from "@firebase/storage";
+import { getDownloadURL, ref, uploadBytesResumable, deleteObject } from "@firebase/storage";
 import { storage } from "../../firebase";
 import { FaCloudUploadAlt } from "react-icons/fa";
+import ProgressBar2 from "../../utility/ProgressBar2";
 
 const Fileupload = ({ handleInputChange }) => {
   const [progress, setProgress] = useState(0);
@@ -13,32 +14,28 @@ const Fileupload = ({ handleInputChange }) => {
     console.log(file);
     uploadfiles(file);
   };
+
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     uploadfiles(file);
   };
 
   const uploadfiles = (file) => {
-    //
     if (!file) {
-      console.log(`ez a probléma`);
+      console.log("Nincs kiválasztott fájl");
       return;
     } else {
-      //ref paraméterei: storage,path in firebase storage,
       const storageref = ref(storage, `/files/${file.name}`);
       const uploadTask = uploadBytesResumable(storageref, file);
-      // on paraméterei: kötött szó,snapshot,error,success
+      
       uploadTask.on(
         "state_changed",
         (snapshot) => {
-          const prog = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          );
+          const prog = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
           setProgress(prog);
         },
         (err) => console.log(err),
         () => {
-          //getDownloadURL paraméterei: path
           getDownloadURL(uploadTask.snapshot.ref).then((url) => {
             handleInputChange(url, "url");
             setImageUrl(url);
@@ -48,33 +45,47 @@ const Fileupload = ({ handleInputChange }) => {
     }
   };
 
+  const deleteImage = () => {
+    if (imageUrl) {
+      const imageRef = ref(storage, imageUrl);
+      
+      deleteObject(imageRef)
+        .then(() => {
+          console.log("Kép sikeresen törölve");
+          setImageUrl("");
+          setProgress(0);
+        })
+        .catch((error) => {
+          console.error("Hiba a kép törlésekor:", error);
+        });
+    }
+  };
+
   return (
-    <div className=" flex flex-col justify-center items-center border-0 border-red-400 w-100 h-100">
+    <div className="flex flex-col justify-center items-center border-0 border-red-400 w-100 h-100">
       {!imageUrl ? (
-        <form
-          onSubmit={formHandler}
-          className="justify-center items-center flex  flex-col border-0"
-        >
-          <label className="  cursor-pointer">
+        <form onSubmit={formHandler} className="justify-center items-center flex flex-col border-0">
+          <label className="cursor-pointer">
             <FaCloudUploadAlt className="w-12 h-12 text-primary" />
             <input
               type="file"
               className="hidden"
-              // ref={fileInputRef}
               onChange={handleImageUpload}
             />
           </label>
         </form>
       ) : (
-        <> <div className="flex flex-col justify-center items-center m-2">
-          <img className="w-96 h-96" src={imageUrl} />
-         
-            <button className="m-2 p-1 px-4 rounded border-0 border-stone-400 text-white bg-red-700">
-              Törlés{" "}
-            </button>
-            <h3 className="text-center">Feltöltve: {progress}%</h3>
-          </div>
-        </>
+        <div className="flex flex-col justify-center items-center m-2">
+          <img className="w-96 h-96" src={imageUrl} alt="Uploaded" />
+          <button
+            className="m-2 p-1 px-4 rounded border-0 border-stone-400 text-white bg-red-700"
+            onClick={deleteImage}
+          >
+            Törlés
+          </button>
+          
+          <ProgressBar2 progress={progress}/>
+        </div>
       )}
     </div>
   );
@@ -82,9 +93,5 @@ const Fileupload = ({ handleInputChange }) => {
 
 export default Fileupload;
 
-/*  <button
-          type="submit"
-          className="m-5 p-1 px-4 rounded border-4 border-stone-400 text-stone-400"
-        >
-          Fájl feltöltése2 ezt kell megszüntetni
-        </button>*/
+
+
---
+++ src/utility/ProgressBar2.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const ProgressBar2 = ({progress}) => {
+  return (
+    <div className="f">
+      <span className="mb-4"><h3 className="text-center">Feltöltve: {progress}%</h3></span>
+      <div className="relative w-1/4 h-2 bg-gray-300">
+        <motion.div
+          className="absolute top-0 left-0 h-full bg-stone-500"
+          initial={{ width: 0 }}
+          animate={{ width: progress }}
+          transition={{ duration: 1 }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar2;
+// <div className="fixed inset-0 flex flex-col items-center justify-center bg-gray-200">
